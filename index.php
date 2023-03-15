93e672945d59761f4f1b17c8b40cbf069792a286
Ahmed Eisa <ahmed.hamdy.eisa@gmail.com>
2023-03-15 14:02:16 +0200

    index page edited

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-welcome ya handsa
-my name is :ahmed eisa 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <p>
+            welcome ya handsa
+            my name is :ahmed eisa 
+        </p>
+</body>
+</html>
